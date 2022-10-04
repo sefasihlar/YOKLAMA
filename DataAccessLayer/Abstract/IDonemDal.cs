5e2563517a283778db50d6183a63391885ad3285
ayyıldız@software <sihlarsefa7@gmail.com>
2022-10-04 21:39:03 +0300

    Template ready

--- DataAccessLayer/Abstract/IDonemDal.cs
+++ DataAccessLayer/Abstract/IDonemDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface IDonemDal:IGenericDal<TBL_BOLUM>
+    public interface IDonemDal:IGenericDal<TBL_DONEM>
     {
     }
 }

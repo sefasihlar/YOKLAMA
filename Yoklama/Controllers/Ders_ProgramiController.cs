c2cd115aeac13b0674617d397405911d2684eb6c
ayyıldız@software <sihlarsefa7@gmail.com>
2022-11-19 23:59:49 +0300

    Student and lesson operation created

---
+++ Yoklama/Controllers/Ders_ProgramiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUILayer.Controllers
+{
+    public class Ders_ProgramiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
